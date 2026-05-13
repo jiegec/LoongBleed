42a6d51ec065a5177ee38e0e18c204a6cfcebc8b
Jiajie Chen <c@jia.je>
2026-05-13 12:58:19 +0800

    Cleanup properly

--- poc_la464.sh
+++ poc_la464.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+trap 'kill $LOOP_PID 2>/dev/null; exit' INT TERM EXIT
+
 for i in $(seq 1 1000); do
     printf "testtest%08d\n" "$i"
 done > /tmp/file
@@ -7,5 +9,7 @@ done > /tmp/file
 sh -c "while true; do
     numactl -C 0 sort < /tmp/file > /dev/null
 done" &
+LOOP_PID=$!
 
 ./run.sh --vld
+wait $LOOP_PID
