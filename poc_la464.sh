d49200fc492ec0efd360bfd08ac2fac4b5c251d9
Jiajie Chen <c@jia.je>
2026-05-13 12:49:09 +0800

    Add poc for la464

---
+++ poc_la464.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+for i in $(seq 1 1000); do
+    printf "testtest%08d\n" "$i"
+done > /tmp/file
+
+sh -c "while true; do
+    numactl -C 0 sort < /tmp/file > /dev/null
+done" &
+
+./run.sh --vld
